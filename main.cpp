0459a465cdd5f34bee9d7122e41719918d85ff75
Charlie <charlie.rasberry@outlook.com>
2024-10-16 23:17:19 +0100

    Started the create hand function

--- main.cpp
+++ main.cpp
@@ -12,10 +12,10 @@ typedef struct card {
 std::vector<card> create_deck() {
     std::vector<card> deck;
     string suits[4] = {"Hearts", "Diamonds", "Clubs", "Spades"};
-    string value[12] = { "2" , "3", "4", "5", "6", "7", "8", "9","Jack","Queen","King", "Ace"};
+    string value[13] = { "2" , "3", "4", "5", "6", "7", "8", "9","10", "Jack","Queen","King", "Ace"};
 
     for (int i = 0; i < 4; i++) {  // Iterate over suits
-        for (int j = 0; j < 12; j++) {  // Iterate over values
+        for (int j = 0; j < 13; j++) {  // Iterate over values
             card c = {suits[i], value[j]};
             deck.push_back(c);
         }
@@ -23,11 +23,24 @@ std::vector<card> create_deck() {
     return deck;
 }
 
+std::vector<card> create_hand(std::vector<card> &deck) {
+    std::vector<card> hand;
+    for (int i = 0; i < deck.size(); ++i) {
+        //  add code to remove from deck to disperse cards to hand
+        return hand;
+    }
+}
+
 int main() {
+    //  Initialise deck
     std::vector<card> deck = create_deck();
-    // Example: print the deck
+
+    // print the deck
     for (const auto& c : deck) {
         cout << c.value << " of " << c.suit << "\n";
     }
+
+
+
     return 0;
 }
